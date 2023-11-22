9474b4db46334c87eb393abf8ec884fc36030ebf
Crystal <crystal120599@gmail.com>
2023-11-22 15:48:27 -0800

    get user id

--- routes/users.js
+++ routes/users.js
@@ -8,3 +8,7 @@ userRouter.get("/", async(req,res) =>{
     let users = await User.findAll();
     res.json(users)
 })
+userRouter.get("/:id", async(req,res) =>{
+    let user = await User.findByPk(req.params.id);
+    res.json(user)
+})
