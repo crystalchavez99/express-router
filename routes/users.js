b210860d22e56e7be1876e6b6f2970e5d4dec419
Crystal <crystal120599@gmail.com>
2023-11-22 15:47:46 -0800

    get user route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const userRouter = express.Router();
 
+const User = require("../models/User")
+
 userRouter.get("/", async(req,res) =>{
-    
+    let users = await User.findAll();
+    res.json(users)
 })
