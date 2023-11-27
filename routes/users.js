49712809c3b8f3930464a6fc1248a0600382c16d
Crystal <crystal120599@gmail.com>
2023-11-27 11:23:54 -0800

    tested postman

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,27 @@ const express = require("express");
 
 const userRouter = express.Router();
 
-const User = require("../models/User")
+const {User} = require("../models/index")
 
 userRouter.get("/", async(req,res) =>{
     let users = await User.findAll();
     res.json(users)
 })
+
 userRouter.get("/:id", async(req,res) =>{
     let user = await User.findByPk(req.params.id);
     res.json(user)
 })
+
 userRouter.post("/", async(req,res) =>{
+    //const { name, age } = req.body;
     let newUser = await User.create(req.body);
     res.json(newUser)
 })
 userRouter.put("/:id", async(req,res) =>{
     let user = await User.findByPk(req.params.id);
     await user.update(req.body)
+    res.json(user)
 })
 userRouter.delete("/:id", async(req,res) =>{
     let user = await User.findByPk(req.params.id);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const userRouter = require("../routes/users")
 
+app.use(express.json())
 app.use("/users", userRouter)
 
 module.exports = app;
